326914a34443ae5be4a4ab484036bac3a8b164db
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-04-29 20:08:43 -0500

    added student data

---
+++ src/helpers/data/StudentData.js
@@ -0,0 +1,179 @@
+const Students = [
+  {
+    id: 1,
+    firstName: 'Lindsey',
+    lastName: 'Satterfield',
+    image: 'https://avatars2.githubusercontent.com/u/23327097?v=4',
+    isDead: false
+  },
+  {
+    id: 2,
+    firstName: 'Ben',
+    lastName: 'Jarrett',
+    image: 'https://avatars0.githubusercontent.com/u/76926244?v=4',
+    isDead: false
+  },
+  {
+    id: 3,
+    firstName: 'Casey',
+    lastName: 'Walker',
+    image: 'https://avatars2.githubusercontent.com/u/76138733?v=4',
+    isDead: false
+  },
+  {
+    id: 4,
+    firstName: 'Chie',
+    lastName: 'Stroud',
+    image: 'https://avatars2.githubusercontent.com/u/68720317?v=4',
+    isDead: false
+  },
+  {
+    id: 5,
+    firstName: 'Chris',
+    lastName: 'Meffley',
+    image: 'https://avatars2.githubusercontent.com/u/76795299?v=4',
+    isDead: false
+  },
+  {
+    id: 6,
+    firstName: 'Dani',
+    lastName: 'Crosby',
+    image: 'https://avatars0.githubusercontent.com/u/68397076?v=4',
+    isDead: false
+  },
+  {
+    id: 7,
+    firstName: 'Ellie',
+    lastName: 'Walker',
+    image: 'https://avatars0.githubusercontent.com/u/76851756?v=4',
+    isDead: false
+  },
+  {
+    id: 8,
+    firstName: 'Martin',
+    lastName: 'Walker',
+    image: 'https://avatars3.githubusercontent.com/u/58958998?v=4',
+    isDead: false
+  },
+  {
+    id: 9,
+    firstName: 'Gabby',
+    lastName: 'Tobermann',
+    image: 'https://avatars3.githubusercontent.com/u/76187279?v=4',
+    isDead: false
+  },
+  {
+    id: 10,
+    firstName: 'Matthew',
+    lastName: 'Gonzales',
+    image: 'https://avatars3.githubusercontent.com/u/16343050?v=4',
+    isDead: false
+  },
+  {
+    id: 11,
+    firstName: 'Matthew',
+    lastName: 'Gonzales',
+    image: 'https://avatars3.githubusercontent.com/u/16343050?v=4',
+    isDead: false
+  },
+  {
+    id: 12,
+    firstName: 'John',
+    lastName: 'Maple',
+    image: 'https://avatars3.githubusercontent.com/u/51683901?v=4',
+    isDead: false
+  },
+  {
+    id: 13,
+    firstName: 'Holly',
+    lastName: 'Parsons',
+    image: 'https://avatars3.githubusercontent.com/u/67122062?v=4',
+    isDead: false
+  },
+  {
+    id: 14,
+    firstName: 'Hunter',
+    lastName: 'Juneau',
+    image: 'https://avatars2.githubusercontent.com/u/20494604?v=4',
+    isDead: false
+  },
+  {
+    id: 15,
+    firstName: 'Jim',
+    lastName: 'Connor',
+    image: 'https://avatars1.githubusercontent.com/u/76793747?v=4',
+    isDead: false
+  },
+  {
+    id: 16,
+    firstName: 'Jesse',
+    lastName: 'Robinson',
+    image: 'https://avatars3.githubusercontent.com/u/45837967?v=4',
+    isDead: false
+  },
+  {
+    id: 17,
+    firstName: 'Juan',
+    lastName: 'Davila',
+    image: 'https://avatars2.githubusercontent.com/u/76821980?v=4',
+    isDead: false
+  },
+  {
+    id: 18,
+    firstName: 'Katy',
+    lastName: 'Fry',
+    image: 'https://avatars2.githubusercontent.com/u/76188832?v=4',
+    isDead: false
+  },
+  {
+    id: 19,
+    firstName: 'Mitchell',
+    lastName: 'Crumbley',
+    image: 'https://avatars0.githubusercontent.com/u/76921222?v=4',
+    isDead: false
+  },
+  {
+    id: 20,
+    firstName: 'Nathan',
+    lastName: 'Keith',
+    image: 'https://avatars2.githubusercontent.com/u/76710923?v=4',
+    isDead: false
+  },
+  {
+    id: 21,
+    firstName: 'Rob',
+    lastName: 'Cole',
+    image: 'https://avatars0.githubusercontent.com/u/76854545?v=4',
+    isDead: false
+  },
+  {
+    id: 22,
+    firstName: 'Sara',
+    lastName: 'Schoonover',
+    image: 'https://avatars3.githubusercontent.com/u/25774314?v=4',
+    isDead: false
+  },
+  {
+    id: 23,
+    firstName: 'Sean',
+    lastName: 'Rossettie',
+    image: 'https://avatars2.githubusercontent.com/u/76828201?v=4',
+    isDead: false
+  },
+  {
+    id: 24,
+    firstName: 'Honey-Rae',
+    lastName: 'Swan',
+    image: 'https://avatars3.githubusercontent.com/u/76716670?v=4',
+    isDead: false
+  },
+  {
+    id: 25,
+    firstName: 'Tad',
+    lastName: 'Sekeres',
+    image: 'https://avatars0.githubusercontent.com/u/74842096?v=4',
+    isDead: false
+  }
+];
+
+export default Students;
